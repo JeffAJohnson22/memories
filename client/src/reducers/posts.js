25d5cecde8948669945e0bb83769b392a1d427d3
Jeffrey Johnson <jeffreyjohnson@Baby-Mama.local>
2020-12-18 03:15:23 -0500

    second commit

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,18 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKEPOST } from '../constants/actionTypes'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (posts = [], action) => {
+  switch (action.type) {
+      case DELETE:
+        return posts.filter((post) => post._id !== action.payload);
+      case UPDATE:
+      case LIKEPOST:
+        return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+      case FETCH_ALL:
+          return action.payload;
+       case CREATE:
+            return [...posts, action.payload]
+      default:
+          return posts
+  }
+}
