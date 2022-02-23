a9b293956e40a2b3da333c6b896de05bb8658897
Jeffrey <Jeffalanjohnson22@gmail.com>
2022-02-23 08:58:23 -0500

    4

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,6 +11,7 @@ export const getPosts = ( ) => async (dispatch) => {
 }
 console.log('ornae')
 console.log('3')
+console.log('3')
 
 export const createPost = (post) => async (dispatch) => {
     try {
