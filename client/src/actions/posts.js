f9985d6f4beaa7f2d3c7b9c6def6931d1fbed4ac
Jeffrey <Jeffalanjohnson22@gmail.com>
2022-02-22 22:23:22 -0500

    practive1

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,7 +9,7 @@ export const getPosts = ( ) => async (dispatch) => {
         console.log(error.message)
     }
 }
-
+console.log('apple')
 export const createPost = (post) => async (dispatch) => {
     try {
         const {data} = await api.createPost(post)
